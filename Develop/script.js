3030d4798fa65ef43aea273ba6f5a29904a0fae5
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-03-20 13:27:41 -0600

    Further work on the code adter Tutoring Session

--- Develop/script.js
+++ Develop/script.js
@@ -4,19 +4,12 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var specialCharacters = "!#$%& ()*+,-./:;<=>?@[]^_`{|}~";
-var numericCharacters = "0123456789";
-var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-console.log (specialCharacters);
-console.log (numericCharacters);
-console.log (lowercaseCharacters);
-console.log (uppercaseCharacters);
+function generatePassword () {
+  var password = '';
 
+  var UserSelection = '';
 
-// Write password to the #password input
-function writePassword() {
   var firstprompt = window.prompt ('How many characters would you like your password to contain?');
   if (!firstprompt) {
     return;
@@ -25,14 +18,68 @@ function writePassword() {
   if (firstprompt < 8 || firstprompt > 128) {
     window.alert ("Please select at least 8 and no more than 128 characters for your password");
     return;
-  } 
-  
-  if (firstprompt >= 8 && firstprompt <= 128) {
+  } else {
     var secondprompt = window.confirm ('Click OK to confirm including special characters.');
     var thirdprompt = window.confirm ('Click OK to confirm including numeric characters.');
     var fourthprompt = window.confirm ('Click OK to confirm including lowercase characters.');
-    var fifthprompt = window.confirm ('Click OK to confirm including uppercase characters.')
+    var fifthprompt = window.confirm ('Click OK to confirm including uppercase characters.');
   }
+    // if (firstprompt >= 8 && firstprompt <= 128) {
+
+  console.log(secondprompt);
+  console.log(thirdprompt);
+  console.log(fourthprompt);
+  console.log(fifthprompt);
+
+  var specialCharacters = "!#$%& ()*+,-./:;<=>?@[]^_`{|}~";
+  var numericCharacters = "0123456789";
+  var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+if (secondprompt === true) {
+  console.log ('SecondPrompt is true');
+  UserSelection = UserSelection.concat(specialCharacters);
+} else {
+  console.log ('SecondPrompt is false');
+  UserSelection = UserSelection;
+}
+
+console.log('second',UserSelection);
+
+if (thirdprompt === true) {
+  UserSelection = UserSelection.concat(numericCharacters);
+} else {
+  UserSelection = UserSelection;
+}
+
+console.log ('third',UserSelection)
+
+if (fourthprompt === true) {
+  UserSelection = UserSelection.concat(lowercaseCharacters);
+} else {
+
+}
+
+console.log ('fourth',UserSelection)
+
+if (fifthprompt === true) {
+  UserSelection = UserSelection.concat(uppercaseCharacters);
+}
+
+console.log ('fifth',UserSelection);
+
+if (secondprompt === false && thirdprompt === false && fourthprompt === false && fifthprompt === false) {
+  window.alert ('Please select at least one type of character for your password');
+  return;
+}
+
+
+  return password;
+
+}
+
+// Write password to the #password input
+function writePassword() {
 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
