830362d8528468c462a65b3efd51e85afc7cc0c7
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-03-22 13:19:08 -0600

    Final changes before deployment, general code was cleaned of unused code lines. Modified the readme file and added an asset that shows the functionality of the page.

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,7 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-
+// Creation of the function to generate the password. Inside we declare a couple of empty variables that will later be filled with functions
 function generatePassword () {
   var password = '';
 
@@ -25,61 +22,55 @@ function generatePassword () {
     var fifthprompt = window.confirm ('Click OK to confirm including uppercase characters.');
   }
 
-  console.log(firstprompt);
-  console.log(secondprompt);
-  console.log(thirdprompt);
-  console.log(fourthprompt);
-  console.log(fifthprompt);
+  // These are console logs created originally to confirm the user selections were boolean, so they can be used further in the code to create statements. Commented Out as they do not provide functionality to the final code but left as a testimony of the process to make this challenge.
+  // console.log(firstprompt);
+  // console.log(secondprompt);
+  // console.log(thirdprompt);
+  // console.log(fourthprompt);
+  // console.log(fifthprompt);
 
+  // Creation of variables that will fill the characterBank variable according to the user selection inside the previous prompts.
   var specialCharacters = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
   var numericCharacters = "0123456789";
   var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  // On these if statements, i left a console log so the user can confirm which type of characters were being added to the password according to their selection on the prompts.
 if (secondprompt === true) {
-  console.log ('Special Characters will be included in the password');
+  console.log ('Special Characters were considered to be included in the password');
   characterBank = characterBank.concat(specialCharacters);
 }
 
 if (thirdprompt === true) {
-  console.log ('Numeric Characters will be included in the password');  
+  console.log ('Numeric Characters were considered to be included in the password');  
   characterBank = characterBank.concat(numericCharacters);
 } 
 
 if (fourthprompt === true) {
-  console.log ('lowercase Characters will be included in the password');
+  console.log ('lowercase Characters were considered to be included in the password');
   characterBank = characterBank.concat(lowercaseCharacters);
 } 
 
 if (fifthprompt === true) {
-  console.log ('uppercase Characters will be included in the password');
+  console.log ('uppercase Characters were considered to be included in the password');
   characterBank = characterBank.concat(uppercaseCharacters);
 }
 
+// This is an alert message in case the user selects a false value in all the prompts. At least one type of character is necessary to create the password.
 if (secondprompt === false && thirdprompt === false && fourthprompt === false && fifthprompt === false) {
   window.alert ('Please select at least one type of character for your password');
   return;
 }
 
-console.log(characterBank);
-console.log(characterBank.length);
-
-// var password = for (characterBank )
+// For loop created so that the password is generated with a random selector of a character type from the characterBank variable each time the iteration applies. The number of times the loop is working is tied to the value of the first prompt the user selected.
 
 for (var i = 0; i < firstprompt; i++) {
   var randomNumber = Math.floor(Math.random() * characterBank.length);
-  console.log(i + '#of times the loop is running');
   password = password.concat(characterBank[randomNumber]);
 }
 
-console.log(randomNumber);
-console.log(password);
-// var randomNumber = Math.floor(Math.random() * characterBank.length);
-// console.log (randomizer);
-// console.log (typeof randomizer);
-
+// We're ending the function so that it returns the value of the password variable, so that the write password function can later be used to "print" the final password onto the page.
   return password;
-
 }
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # 03-BC-Password-Challenge
-Repo created for a Password generator made with JS
+This repo was created for a Password generator webpage made with JavaScript, with style being defined by a CSS file and the webpage structure by a html file.
+
+## Description
+
+This web application is a password generator that responds to user selections via window promts, and generates a random password according to those selections.
+
+## Mockup
+
+The site should have the functionality described in the following GIF image.
+
+![alt text](./Develop/assets/img/pagemockup.gif)
+
+## Link
+
+Follow this URL to access the website: https://alextrejo92.github.io/03-BC-Password-Challenge/
+
+## Installation
+
+N/A
+
+## Credits
+
+The web page design was provided by EdX bootcamps & Tec de Monterrey. The functionality of the page was coded by me.
+
+## License
+
+Standar MIT license.
